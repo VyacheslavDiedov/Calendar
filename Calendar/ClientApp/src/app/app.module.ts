e72f84c33824f10fc69a73f91ea35d03919ab09e
didusenko <didusenko.dmytro@gmail.com>
2020-12-08 03:49:47 +0200

    Added calendar component: initial layout, scrolling to the top of the page while moving by pages

--- Calendar/ClientApp/src/app/app.module.ts
+++ Calendar/ClientApp/src/app/app.module.ts
@@ -5,37 +5,42 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { TestPageComponent } from './test-page/test-page.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { HeaderComponent} from './header/header.component';
 import { RegisterDialogComponent } from './register-dialog/register-dialog.component';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { LoginDialogComponent } from './login-dialog/login-dialog.component';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { FooterComponent } from './footer/footer.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
-        TestPageComponent,
         HeaderComponent,
         RegisterDialogComponent,
         LoginDialogComponent,
-        FooterComponent
+        FooterComponent,
+        CalendarComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
         FormsModule,
         RouterModule.forRoot([
-            { path: '', component: HomeComponent, pathMatch: 'full' },
-            { path: 'test', component: TestPageComponent }
+            {path: '', redirectTo: '/home', pathMatch: 'full'},
+            {path: 'home', component: HomeComponent},
+            {path: 'calendar', component: CalendarComponent}
         ]),
         MatToolbarModule,
         MatIconModule,
-        MatDialogModule
+        MatDialogModule,
+        MatButtonModule,
+        MatMenuModule
     ],
     providers: [],
     bootstrap: [AppComponent]
