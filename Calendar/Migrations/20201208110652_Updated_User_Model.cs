64b16186f9fe2b00815d9ade534601e31c3a800b
didusenko <didusenko.dmytro@gmail.com>
2020-12-08 14:00:47 +0200

    User DB model migrations

---
+++ Calendar/Migrations/20201208110652_Updated_User_Model.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Calendar.Migrations
+{
+    public partial class Updated_User_Model : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrganizationName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Token",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Users",
+                newName: "UserPhone");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserFirstName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserLastName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserFirstName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserLastName",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "UserPhone",
+                table: "Users",
+                newName: "UserName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OrganizationName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Token",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
